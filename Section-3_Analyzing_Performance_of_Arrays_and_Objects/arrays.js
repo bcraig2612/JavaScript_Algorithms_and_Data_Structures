6dad48f810c92c11ef0e76eec565bafb23ad9464
Brandon Craig <bcraig2612@gmail.com>
2019-12-11 14:52:08 -0700

    Onto Problem Solving Section

---
+++ Section-3_Analyzing_Performance_of_Arrays_and_Objects/arrays.js
@@ -0,0 +1,1 @@
+let names = ['Brandon', 'Ethan', 'Christian', 'Brian'];
---
+++ Section-3_Analyzing_Performance_of_Arrays_and_Objects/objects.js
@@ -0,0 +1,5 @@
+let instructor = {
+    firstName: "Brandon",
+    isInstructor: true,
+    favoriteNumbers: [ 1, 2, 3, 4 ]
+}
