9d6662d4cc180269dce52d96681d2f4b0529cd79
Brandon Craig <bcraig2612@gmail.com>
2019-12-18 22:32:13 -0700

    Finished Section-21

--- Section-21_Stacks_&_Queues/Queue.js
+++ Section-21_Stacks_&_Queues/Queue.js
@@ -1,25 +1,48 @@
-// ENQUEUE
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
-// This function accepts some value
+class Queue {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+  // This function accepts some value
+  enqueue(val) {
+    // Create a new node using that value passed to the function
+    var newNode = new Node(val);
+    // If there are no nodes in the queue, set this node to be the first and last property of the queue
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+      // Otherwise, set the next property on the current last to be that node, and then set the last property of the queue to be that node
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    // Increment the size of the queue by 1
+    return ++this.size;
+  }
 
-// Create a new node using that value passed to the function
+  dequeue() {
+    // If there is no first property, just return null
+    if (!this.first) return null;
 
-// If there are no nodes in the queue, set this node to be the first and last property of the queue
-
-// Otherwise, set the next property on the current last to be that node, and then set the last property of the queue to be that node
-
-// Increment the size of the queue by 1
-
-// DEQUEUE
-
-// If there is no first property, just return null
-
-// Store the first property in a variable
-
-// See if the first is the same as the last (check if there is only 1 node). If so, set the first and last to be null
-
-// If there is more than 1 node, set the first property to be the next property of first
-
-// Decrement the size by 1
-
-// Return the value of the node dequeued
+    // Store the first property in a variable
+    var temp = this.first;
+    // See if the first is the same as the last (check if there is only 1 node). If so, set the first and last to be null
+    if (this.first === this.last) {
+      this.last = null;
+    }
+    // If there is more than 1 node, set the first property to be the next property of first
+    this.first = this.first.next;
+    // Decrement the size by 1
+    this.size--;
+    // Return the value of the node dequeued
+    return temp.value;
+  }
+}
